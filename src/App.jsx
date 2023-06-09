236ca6b883f1aa323765ea53a8b511722682827f
Danny Cho <danheecho@gmail.com>
2023-06-08 18:14:07 -0700

    Submit handler implemented. Submitted entries now populate under pending TDL.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
 	const [newItem, setNewItem] = useState("");
 	const [todos, setTodos] = useState([]);
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(`Submitted ${newItem}!!!`)
-    setNewItem("");
-  }
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setTodos((currentTodos) => {
+			return [
+				...currentTodos,
+				{
+					id: crypto.randomUUID(),
+					title: newItem,
+					completed: false,
+				},
+			];
+		});
+		console.log(`Submitted ${newItem}!!!`);
+		console.log(todos);
+		setNewItem("");
+	};
 
 	return (
 		<>
@@ -36,26 +48,19 @@ const App = () => {
 				<div className="lists-div__pending-list-div">
 					<h2>Pending TDL</h2>
 					<ul>
-						<li>
-							<p>Test TD 1</p>
-							<button>Done</button>
-							<button>Remove</button>
-						</li>
-						<li>
-							<p>Test TD 1</p>
-							<button>Done</button>
-							<button>Remove</button>
-						</li>
-						<li>
-							<p>Test TD 1</p>
-							<button>Done</button>
-							<button>Remove</button>
-						</li>
-						<li>
-							<p>Test TD 1</p>
-							<button>Done</button>
-							<button>Remove</button>
-						</li>
+						{todos
+							.filter((todo) => {
+								return todo.completed === false;
+							})
+							.map((todo) => {
+								return (
+									<li key={todo.id}>
+										<p>{todo.title}</p>
+										<button>Done</button>
+										<button>Remove</button>
+									</li>
+								);
+							})}
 					</ul>
 				</div>
 				<div className="lists-div__completed-list-div">
